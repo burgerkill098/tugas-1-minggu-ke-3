6bdf7c8880f3ef5ece827e56a7447129b153062c
burgerkill098 <115003776+burgerkill098@users.noreply.github.com>
2022-10-22 15:51:51 +0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,22 @@
+
+import os
+
+print("waktu  =")
+t=int(input())
+print("kecepatan =")
+v= int(input())
+j=v*t
+
+os.system('cls')
+
+
+
+
+print("                  DATA KECEPATAN MOBIL ")
+print("___________________________________________________________")
+
+print("KECEPATAN RATA RATA (KM/JAM) :",v)
+print("Waktu tempu (jam ) : ",t)
+print("JARAK TEMPU  :",j )
+
+print("___________________________________________________________")
