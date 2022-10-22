92829463ae5ee66399d4d3bb4623f51b1a9e3989
burgerkill098 <115003776+burgerkill098@users.noreply.github.com>
2022-10-22 15:49:45 +0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,22 @@
+import os
+
+nama=input("masukkan nama :")
+nim=input("masukkan nim :")
+jurusan=input("masukkan jurusan  :")
+fakultas=input("msukkan fakultas  :")
+
+os.system('cls')
+
+
+print("______________________________________________________________________")
+
+print("            UNIVERSITAS TEKNOLOGI SUMBAWA ")
+print("            JL. Olat Maras ,SUMBAWA BESAR ")
+print("______________________________________________________________________")
+
+print("Nama     :",nama)
+print("NIM      :",nim)
+print("Jurusan  :",jurusan )
+print("Fakultas :",fakultas )
+
+print("______________________________________________________________________")
